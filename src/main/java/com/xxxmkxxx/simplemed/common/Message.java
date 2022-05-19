b30fba150d030d260ecad43989b87a2de973a66a
xxxmkxxx <maks88648@gmail.com>
2022-05-19 19:00:56 +0400

    Добавил класс сообщения для возвращения в Response ответе сервера.

---
+++ src/main/java/com/xxxmkxxx/simplemed/common/Message.java
@@ -0,0 +1,16 @@
+package com.xxxmkxxx.simplemed.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class Message {
+    private String text;
+    private MessageType type;
+
+    public enum MessageType {
+        MESSAGE, ERROR, WARNING;
+    }
+}
