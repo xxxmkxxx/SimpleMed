2f433f4d2e65bd0c27489f5d015c9a9a6e6c72a1
xxxmkxxx <maks88648@gmail.com>
2022-05-19 18:49:06 +0400

    Добавил метод возращающий массив названий профессий

--- src/main/java/com/xxxmkxxx/simplemed/common/Professions.java
+++ src/main/java/com/xxxmkxxx/simplemed/common/Professions.java
@@ -3,6 +3,9 @@ package com.xxxmkxxx.simplemed.common;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor
 public enum Professions {
     SURGEON("Хирург"),
@@ -12,5 +15,15 @@ public enum Professions {
     ONCOLOGIST("Онколог");
 
     @Getter
-    private String name;
+    private final String name;
+
+    public static String[] getProfessionNames() {
+        List<String> names = new ArrayList<>();
+
+        for (Professions profession : Professions.values()) {
+            names.add(profession.getName());
+        }
+
+        return names.toArray(new String[Professions.values().length]);
+    }
 }
