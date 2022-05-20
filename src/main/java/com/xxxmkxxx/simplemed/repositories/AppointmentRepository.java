4a66b4c4f48e1cbff15a51a95e7e970e4bc7cfee
xxxmkxxx <maks88648@gmail.com>
2022-05-20 14:43:33 +0400

    Удалил метод getAppointmentModelByDateAndTimeAndMedic; Добавил метод existsAppointmentModelByDateAndTimeAndMedic для проверки существования записи; Поменял классы для работы с датой и временем.

--- src/main/java/com/xxxmkxxx/simplemed/repositories/AppointmentRepository.java
+++ src/main/java/com/xxxmkxxx/simplemed/repositories/AppointmentRepository.java
@@ -1,18 +1,18 @@
 package com.xxxmkxxx.simplemed.repositories;
 
-import com.xxxmkxxx.simplemed.common.Professions;
 import com.xxxmkxxx.simplemed.models.AppointmentModel;
 import com.xxxmkxxx.simplemed.models.MedicalStaffModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentModel, Integer> {
-    AppointmentModel getAppointmentModelByDateAndTimeAndMedic(Date date, Date time, MedicalStaffModel medic);
+    boolean existsAppointmentModelByDateAndTimeAndMedic(LocalDate date, LocalTime time, MedicalStaffModel medic);
 
-    List<AppointmentModel> getAllByDateAndMedic_Profession(Date date, Professions profession);
+    List<AppointmentModel> getAllByDateAndMedic(LocalDate date, MedicalStaffModel medic);
 }
 
