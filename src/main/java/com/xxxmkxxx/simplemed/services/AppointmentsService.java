2171ad890e4444c70aa1605e52882049d1b41044
xxxmkxxx <maks88648@gmail.com>
2022-05-21 16:24:43 +0400

    перенёс логику создания записи в метод createAppointment; перегрузил метод createAppointment для поддержки создания записи к свободному врачу из списка врачей

--- src/main/java/com/xxxmkxxx/simplemed/services/AppointmentsService.java
+++ src/main/java/com/xxxmkxxx/simplemed/services/AppointmentsService.java
@@ -22,26 +22,40 @@ import java.util.List;
 public record AppointmentsService(
         AppointmentRepository appointmentRepository
 ) {
-    public Message safeAppointment(AppointmentModel appointment) {
-        if (!isWorkingTime(appointment.getTime(), appointment.getMedic())) {
+    public Message createAppointment(LocalDateTime dateTime, MedicalStaffModel medic, PatientModel patient) {
+        if (!isWorkingTime(dateTime.toLocalTime(), medic)) {
             return new Message("Невозможно сделать запись вне рабочего времени врача!", Message.MessageType.ERROR);
-        } else if (!isCorrectTimeInterval(appointment.getTime())) {
+        } else if (!isCorrectTimeInterval(dateTime.toLocalTime())) {
             return new Message("Невозможно сделать запись из-за некорректного интервала времени!", Message.MessageType.ERROR);
-        } else if (!isAppointmentFree(appointment)) {
-            return new Message("Невозможно записать несколько человек на одно и тоже время!", Message.MessageType.ERROR);
+        } else if (!isAppointmentFree(dateTime.toLocalDate(), dateTime.toLocalTime(), medic)) {
+            return new Message("Невозможно записать несколько человек на одно и тоже время к одному врачу!", Message.MessageType.ERROR);
         }
+
+        AppointmentModel appointment = AppointmentModel.builder()
+                        .patient(patient).medic(medic)
+                        .date(dateTime.toLocalDate()).time(dateTime.toLocalTime())
+                        .build();
 
         appointmentRepository.save(appointment);
 
-        return new Message("Запись успешно создана!", Message.MessageType.MESSAGE);
+        return new Message("Запись успешно создана!", Message.MessageType.SUCCESS);
     }
 
-    private boolean isAppointmentFree(AppointmentModel appointment) {
-        return !appointmentRepository.existsAppointmentModelByDateAndTimeAndMedic(
-                appointment.getDate(),
-                appointment.getTime(),
-                appointment.getMedic()
-        );
+    public Message createAppointment(LocalDateTime dateTime, List<MedicalStaffModel> medics, PatientModel patient) {
+        Message message = new Message("none", Message.MessageType.ERROR);
+
+        for (MedicalStaffModel medic : medics) {
+            message = createAppointment(dateTime, medic, patient);
+
+            if (message.getType() == Message.MessageType.SUCCESS)
+                return message;
+        }
+
+        return message;
+    }
+
+    private boolean isAppointmentFree(LocalDate date, LocalTime time, MedicalStaffModel medic) {
+        return !appointmentRepository.existsAppointmentModelByDateAndTimeAndMedic(date, time, medic);
     }
 
     private boolean isCorrectTimeInterval(LocalTime time) {
