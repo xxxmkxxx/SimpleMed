ec87191f08f8dcbf132d28fdfe0c34f7709401b6
xxxmkxxx <maks88648@gmail.com>
2022-05-28 13:24:41 +0400

    Подключил spring security к проекту

---
+++ src/main/java/com/xxxmkxxx/simplemed/configs/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.xxxmkxxx.simplemed.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/patient/**").authenticated()
+                .and()
+                .logout().logoutSuccessUrl("/");
+    }
+}
