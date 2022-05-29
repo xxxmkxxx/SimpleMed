8173157f0310f099472dd163927ecec3a46f66b1
xxxmkxxx <maks88648@gmail.com>
2022-05-29 15:49:28 +0400

    Добавил passwordEncoder и daoAuthenticationProvider

--- src/main/java/com/xxxmkxxx/simplemed/configs/SecurityConfig.java
+++ src/main/java/com/xxxmkxxx/simplemed/configs/SecurityConfig.java
@@ -1,18 +1,42 @@
 package com.xxxmkxxx.simplemed.configs;
 
+import com.xxxmkxxx.simplemed.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebMvc
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/patient/**").authenticated()
                 .and()
+                .formLogin()
+                .and()
                 .logout().logoutSuccessUrl("/");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userService);
+
+        return daoAuthenticationProvider;
     }
 }
