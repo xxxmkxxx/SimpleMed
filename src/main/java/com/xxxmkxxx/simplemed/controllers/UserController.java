bbc839ac53e7c51de1f1d049f0310e31aa655c7d
xxxmkxxx <maks88648@gmail.com>
2022-06-01 13:33:50 +0400

    Add controller UserController

---
+++ src/main/java/com/xxxmkxxx/simplemed/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.xxxmkxxx.simplemed.controllers;
+
+import com.xxxmkxxx.simplemed.common.Message;
+import com.xxxmkxxx.simplemed.requests.CreateUserRequest;
+import com.xxxmkxxx.simplemed.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Message> register(@RequestBody CreateUserRequest createUserRequest) {
+        Message message = userService.createUser(createUserRequest);
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+}
