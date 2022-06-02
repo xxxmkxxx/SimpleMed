e1d712669e2d4df391c9608e66bab4e3dd5286e4
xxxmkxxx <maks88648@gmail.com>
2022-06-02 18:24:41 +0400

    Add admin prefix for admin role

--- src/main/java/com/xxxmkxxx/simplemed/configs/SecurityConfig.java
+++ src/main/java/com/xxxmkxxx/simplemed/configs/SecurityConfig.java
@@ -4,13 +4,11 @@ import com.xxxmkxxx.simplemed.features.user.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @RequiredArgsConstructor
@@ -23,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/index").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN", "SUPER_ADMIN")
                 .antMatchers("/patient/card", "/get").hasAnyRole("USER", "STAFF", "SUPER_ADMIN")
                 .antMatchers("/patient/appointments").hasAnyRole("USER", "SUPER_ADMIN")
                 .antMatchers("/staff/get").hasAnyRole("USER", "STAFF", "SUPER_ADMIN")
