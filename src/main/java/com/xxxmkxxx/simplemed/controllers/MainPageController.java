daec29e8395a9a236d99a99582ee591e3f4ec56b
xxxmkxxx <maks88648@gmail.com>
2022-06-02 09:49:56 +0400

    Refactoring

--- src/main/java/com/xxxmkxxx/simplemed/controllers/MainPageController.java
+++ src/main/java/com/xxxmkxxx/simplemed/controllers/MainPageController.java
@@ -1,9 +1,7 @@
 package com.xxxmkxxx.simplemed.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
