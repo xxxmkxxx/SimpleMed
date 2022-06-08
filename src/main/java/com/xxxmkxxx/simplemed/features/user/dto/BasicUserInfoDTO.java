8144f80413def8627dd639721a0420c482e956af
xxxmkxxx <maks88648@gmail.com>
2022-06-08 17:22:53 +0400

    Add BasicUserInfoDTO

---
+++ src/main/java/com/xxxmkxxx/simplemed/features/user/dto/BasicUserInfoDTO.java
@@ -0,0 +1,13 @@
+package com.xxxmkxxx.simplemed.features.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class BasicUserInfoDTO {
+    private String name;
+    private String surname;
+    private String patronymic;
+}
